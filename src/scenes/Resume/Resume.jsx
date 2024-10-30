475600f78fc024e94672128ab6f08cc6febaeaf1
Apratim Dutta <2021itb089.apratim@students.iiests.ac.in>
2024-10-31 04:57:36 +0530

    resume link

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -11,7 +11,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const resumeLink =
-  'https://raw.githubusercontent.com/apru02/apru02/main/APRATIM_DUTTA_PLUSH_CV.pdf';
+  'https://raw.githubusercontent.com/apru02/apru02/main/APRATIM_DUTTA_CV_FULLSTACK.pdf';
 
 const Resume = () => {
   const pdfWrapper = useRef(null);
